8c997f659aaace7c424d0ac5e544278a8ecace67
matymarek <matyas2016doma@gmail.com>
2024-03-09 16:51:41 +0100

    fast clip fix

--- src/render/Renderer.java
+++ src/render/Renderer.java
@@ -37,10 +37,6 @@ public class Renderer {
             Mat4 mat = solid.getModel().mul(view).mul(proj);
             switch (part.getType()){
                 case POINTS:
-                    //for(int i = 0; i < part.getCount(); i++) {
-                        //Vertex a = solid.getVertexBuffer().get(solid.getIndexBuffer().get(part.getStart()));
-                        //rasterize somehow
-                    //}
                     break;
                 case LINES:
                     startIndex = part.getStart();
@@ -96,14 +92,14 @@ public class Renderer {
     private void clipTriangle(Vertex a, Vertex b, Vertex c, boolean withTexture){
 
         if ((!((-a.getW()) <= a.getIntX() && a.getIntX() <= a.getW())
-                || !((-a.getW()) <= a.getIntY() && a.getIntY() <= a.getW())
-                || !(0 <= a.getPos().getZ() && a.getPos().getZ() <= a.getW()))
-                || (!((-b.getW()) <= b.getIntX() && b.getIntX() <= b.getW())
-                || !((-b.getW()) <= b.getIntY() && b.getIntY() <= b.getW())
-                || !(0 <= b.getPos().getZ() && b.getPos().getZ() <= b.getW()))
-                || (!((-c.getW()) <= c.getIntX() && c.getIntX() <= c.getW())
-                || !((-c.getW()) <= c.getIntY() && c.getIntY() <= b.getW())
-                || !(0 <= c.getPos().getZ() && c.getPos().getZ() <= c.getW()))) { return; }
+                && !((-a.getW()) <= a.getIntY() && a.getIntY() <= a.getW())
+                && !(0 <= a.getPos().getZ() && a.getPos().getZ() <= a.getW()))
+                && (!((-b.getW()) <= b.getIntX() && b.getIntX() <= b.getW())
+                && !((-b.getW()) <= b.getIntY() && b.getIntY() <= b.getW())
+                && !(0 <= b.getPos().getZ() && b.getPos().getZ() <= b.getW()))
+                && (!((-c.getW()) <= c.getIntX() && c.getIntX() <= c.getW())
+                && !((-c.getW()) <= c.getIntY() && c.getIntY() <= b.getW())
+                && !(0 <= c.getPos().getZ() && c.getPos().getZ() <= c.getW()))) { return; }
         if (a.getPos().getZ() < b.getPos().getZ()) {
             Vertex temp = a; a = b; b = temp;
         }
@@ -137,11 +133,11 @@ public class Renderer {
 
     private void clipLine(Vertex a, Vertex b){
         if ((!((-a.getW()) <= a.getIntX() && a.getIntX() <= a.getW())
-                || !((-a.getW()) <= a.getIntY() && a.getIntY() <= a.getW())
-                || !(0 <= a.getPos().getZ() && a.getPos().getZ() <= a.getW()))
-                || (!((-b.getW()) <= b.getIntX() && b.getIntX() <= b.getW())
-                || !((-b.getW()) <= b.getIntY() && b.getIntY() <= b.getW())
-                || !(0 <= b.getPos().getZ() && b.getPos().getZ() <= b.getW()))) { return; }
+                && !((-a.getW()) <= a.getIntY() && a.getIntY() <= a.getW())
+                && !(0 <= a.getPos().getZ() && a.getPos().getZ() <= a.getW()))
+                && (!((-b.getW()) <= b.getIntX() && b.getIntX() <= b.getW())
+                && !((-b.getW()) <= b.getIntY() && b.getIntY() <= b.getW())
+                && !(0 <= b.getPos().getZ() && b.getPos().getZ() <= b.getW()))) { return; }
         if (a.getPos().getZ() < b.getPos().getZ()) {
             Vertex temp = a; a = b; b = temp;
         }
